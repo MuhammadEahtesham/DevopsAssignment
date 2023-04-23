cac53f2b6f259c1ee44b79ff2b5514382eb61b94
MuhammadEahtesham <k190298@nu.edu.pk>
2023-04-23 10:21:12 +0500

    DevopsAssignment

---
+++ src/components/education.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default class Education extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="education">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-12">
+                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                <div className="col-md-12">
+                    <div className="about-desc">
+                    <span className="heading-meta">Education</span>
+                    <h2 className="colorlib-heading">What I have done?</h2>
+                    <p>1.Matric from BVS Parsi High School(2007-2017)</p>
+                    <p>2.Inter from Adamjee Govt. College(2017-2019) </p>
+                    <p>3. Currently doing Bachelors in Computer Science(BSCS) from FAST NUCES Karachi (2019-Present) </p>
+                
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+        
+      </div>
+    )
+  }
+}
